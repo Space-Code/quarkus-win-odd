d8b34581d02594296787d4d259b84af62d86f984
gabidevjava <zooiv3rde@gmail.com>
2020-09-12 14:53:27 -0300

    add contato no swagger

--- src/main/java/config/MyApplication.java
+++ src/main/java/config/MyApplication.java
@@ -3,9 +3,10 @@ package config;
 import javax.ws.rs.core.Application;
 
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
 
-@OpenAPIDefinition(info = @Info(description = "Cálculos de Odds - Trader Esportivo", title = "Win milionário", version = "1.0.0-SNAPSHOT"))
+@OpenAPIDefinition(info = @Info(description = "Cálculos de Odds - Trader Esportivo", title = "Win milionário", version = "1.0.0-SNAPSHOT", contact = @Contact(name = "Gabriela Guedes", email = "gabidevenginner@gmail.com")))
 public class MyApplication extends Application {
 	
 }
