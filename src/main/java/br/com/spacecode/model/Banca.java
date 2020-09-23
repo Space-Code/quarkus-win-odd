5b71f5460c02dceffc66a6d45520b79d22c400c5
gabidevjava <zooiv3rde@gmail.com>
2020-09-23 19:27:40 -0300

    retirando id da banca

--- src/main/java/br/com/spacecode/model/Banca.java
+++ src/main/java/br/com/spacecode/model/Banca.java
@@ -3,9 +3,6 @@ package br.com.spacecode.model;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
@@ -13,10 +10,6 @@ import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 @Entity
 @Table(name = "banca")
 public class Banca extends PanacheEntityBase {
-	
-	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long id;
 	
 	private BigDecimal valorBanca;
 
@@ -29,14 +22,5 @@ public class Banca extends PanacheEntityBase {
 	public void setValorBanca(BigDecimal valorBanca) {
 		this.valorBanca = valorBanca;
 	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
 	
 }
