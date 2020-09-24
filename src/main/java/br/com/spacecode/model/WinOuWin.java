59d95880114b90d51a266b862d4295c2ed999fb6
gabidevjava <zooiv3rde@gmail.com>
2020-09-24 13:33:54 -0300

    fix entidades

--- src/main/java/br/com/spacecode/model/WinOuWin.java
+++ src/main/java/br/com/spacecode/model/WinOuWin.java
@@ -18,29 +18,11 @@ public class WinOuWin extends PanacheEntityBase {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
 	
-	private String corretoraA;
-	private String corretoraB;
 	private BigDecimal oddA;
 	private BigDecimal oddB;
 	private String tag;
 	
 	public WinOuWin() {}
-
-	public String getCorretoraA() {
-		return corretoraA;
-	}
-
-	public void setCorretoraA(String corretoraA) {
-		this.corretoraA = corretoraA;
-	}
-
-	public String getCorretoraB() {
-		return corretoraB;
-	}
-
-	public void setCorretoraB(String corretoraB) {
-		this.corretoraB = corretoraB;
-	}
 
 	public BigDecimal getOddA() {
 		return oddA;
