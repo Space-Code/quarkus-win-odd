0516fc2e3563cc1afd69cc2f85c79df470288745
gabidevjava <zooiv3rde@gmail.com>
2020-09-24 15:36:02 -0300

    add outros endpoint da banca

--- src/main/java/br/com/spacecode/resource/BancaResource.java
+++ src/main/java/br/com/spacecode/resource/BancaResource.java
@@ -6,9 +6,12 @@ import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -24,10 +27,10 @@ import br.com.spacecode.service.BancaService;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class BancaResource {
-	
+
 	@Inject
 	BancaService service;
-	
+
 	@GET
 	@Path("/search")
 	@Transactional
@@ -36,12 +39,39 @@ public class BancaResource {
 		return service.findAll();
 	}
 
+	@GET
+	@Path("/search/{id}")
+	@Transactional
+	@Operation(description = "Busca banca cadastrada no sistema pelo id", summary = "Busca banca pelo id")
+	public Banca findById(@PathParam("id") Long id) {
+		return service.findById(id);
+	}
+
 	@POST
 	@Path("/save")
 	@Transactional
 	@Operation(description = "Cadastra banca no sistema", summary = "Cadastra banca")
 	public Response saveBanca(@Valid Banca banca) {
 		return service.save(banca);
+	}
+
+	@PUT
+	@Path("/update/{id}")
+	@Transactional
+	@Operation(description = "Atualiza banca no sistema pelo id", summary = "Atualiza banca pelo id")
+	public Response update(@PathParam("id") Long id, @Valid Banca banca) {
+
+		Banca bancaa = service.update(id, banca);
+
+		return Response.ok(bancaa).build();
+	}
+
+	@DELETE
+	@Path("/delete/{id}")
+	@Transactional
+	@Operation(description = "Deleta banca no sistema pelo id", summary = "Deleta banca pelo id")
+	public Response deleteUser(@PathParam("id") Long id) {
+		return service.delete(id);
 	}
 
 }
--- src/main/java/br/com/spacecode/service/BancaService.java
+++ src/main/java/br/com/spacecode/service/BancaService.java
@@ -3,6 +3,7 @@ package br.com.spacecode.service;
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 
 import br.com.spacecode.model.Banca;
@@ -14,9 +15,30 @@ public class BancaService {
 		return Banca.findAll().list();
 	}
 
+	public Banca findById(Long id) {
+		return Banca.findById(id);
+	}
+
 	public Response save(Banca banca) {
 		banca.persist();
 		return Response.status(201).build();
 	}
 
+	public Banca update(Long id, Banca banca) {
+		Banca bancaEntidade = Banca.findById(id);
+
+		if (bancaEntidade == null) {
+			throw new WebApplicationException("Esse " + id + " não existe.", Response.Status.NOT_FOUND);
+		}
+
+		bancaEntidade.setValorBanca(banca.getValorBanca());
+
+		return bancaEntidade;
+	}
+
+	public Response delete(Long id) {
+		Banca banca = Banca.findById(id);
+		banca.delete();
+		return Response.status(201).build();
+	}
 }
